9a98555d10d1eb3a8a5455b223bf76afa3e5fdb2
wenderson guedes <wendersonguedes6@gmail.com>
2024-09-17 12:56:23 -0300

    feat: seção com states, events e forms

---
+++ 04-steps/src/App.js
@@ -0,0 +1,26 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+export default function App() {
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">{messages[step - 1]}</p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Previous</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Next</button>
+      </div>
+
+    </div>
+  );
+}
