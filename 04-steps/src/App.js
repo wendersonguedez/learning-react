4f93714c966781ff985de056467a71b207832053
wenderson <wendersonguedes6@gmail.com>
2024-11-03 17:01:12 -0300

    feat: implementada logica para manipular botão de fechamento

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -14,6 +14,7 @@ export default function App() {
  *      e o componente será re-renderizado com o valor atualizado de step.
  */
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handleNext() {
     if (step < 3) {
@@ -30,24 +31,28 @@ export default function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+        <div className="steps" style={{ display: isOpen ? 'block' : 'none' }}>
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
 
-      <p className="message">{messages[step - 1]}</p>
+          <p className="message">{messages[step - 1]}</p>
 
-      <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
-          Previous
-        </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-
-    </div>
+          <div className="buttons">
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
